97b19ea32d80de0e4fa9ab8f3d2ebe6ff4785c62
Jimmy Aumard <Jimmy.Aumard@nestle.com>
2017-07-30 12:25:26 +0200

    Fix tests

--- test/unit/services/PluginService.test.js
+++ test/unit/services/PluginService.test.js
@@ -9,7 +9,7 @@ const expect = chai.expect
 describe('PluginService', () => {
   const pluginName = 'lisa-plugin-example'
   const internalPluginName = 'example'
-  const version = '0.0.4'
+  const version = '0.0.7'
 
   let service
   it('should exist', () => {
